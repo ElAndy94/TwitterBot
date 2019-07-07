3b5243fbb15509a36c73441072dc46f763752854
Andrew <andrewpeliza@hotmail.com>
2019-07-07 18:02:15 +0100

    Finished Twitter Bot

--- app.py
+++ app.py
@@ -26,13 +26,23 @@ class TwitterBot:
     def like_tweet(self, hashtag):
         bot = self.bot
         bot.get('https://twitter.com/hashtag/'+hashtag+'?src=rela')
-        bot.sleep(3)
+        time.sleep(3)
         for i in range(1, 3):
             bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
             time.sleep(2)
             tweets = bot.find_elements_by_class_name('tweet')
+            links = [elem.get_attribute('data-permalink-path')
+                     for elem in tweets]
+            print(links)
+            for link in links:
+                bot.get('https://twitter.com' + link)
+                try:
+                    bot.find_element_by_class_name('HeartAnimation').click()
+                    time.sleep(30)
+                except Exception as ex:
+                    time.sleep(60)
 
 
-andrew = TwitterBot('user', 'password')
+andrew = TwitterBot('username', 'password')
 andrew.login()
 andrew.like_tweet('webdevelopment')
